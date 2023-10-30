1f74355c77c11049f48a32ed3413c5a1864d7d20
Stradalec <pochtatipo6@gmail.com>
2023-10-30 19:34:17 +0700

    Added recording to file

--- SixthTask.cpp
+++ SixthTask.cpp
@@ -1,11 +1,11 @@
-/**********************************
-* Автор: Калинин Андрей           *
-* Лабораторная №6                 *
-* Дата: 25.10.2023                *
-* Название:Работа с  файлами      *
-* Вариант: 7                      *
-* https://onlinegdb.com/zlo8frfq8 *
-**********************************/
+/*************************************
+* Автор: Калинин Андрей              *
+* Лабораторная №6                    *
+* Дата: 25.10.2023                   *
+* Название:Работа с  файлами         *
+* Вариант: 7                         *
+* https://onlinegdb.com/tXL_R18Dd    *
+*************************************/
 
 #define _CRT_SECURE_NO_DEPRECATE
 
@@ -29,31 +29,31 @@ using namespace std;
 
 //This function help next function work (Gettign string count)
 
-int lineCount (string fileName) {
+int lineCount(string fileName) {
   int lineCounter = 0;
-  
-  ifstream lineCount (fileName);
-  
-  if (!lineCount.is_open ()) {
+
+  ifstream lineCount(fileName);
+
+  if (!lineCount.is_open()) {
     return -1;
   }
-  
+
   while (!lineCount.eof()) { //.eof = end of the file, reading until end of the file
     string countTemp;
     getline(lineCount, countTemp);
-    ++lineCounter;      
+    ++lineCounter;
   }
-  
+
   lineCount.close();
-  
+
   return lineCounter;
 }
 
 
-//This function do all work.  I created this, because it can be used two or more time.
+int main() {
 
-void baseOfStudents (int counter, string fileName) {
-  
+  string firstCrutch = "Most failing student: ";
+  string secondCrutch = " ";
   int firstNumber;
   int secondNumber;
   int thirdNumber;
@@ -61,80 +61,115 @@ void baseOfStudents (int counter, string fileName) {
   double studentAverageScore;
   char temp[30];
   struct student studentsArray[100];
-  
-  ifstream studentsFile (fileName);
-
-  if (!studentsFile.is_open ())
-    {
-      printf ("Error: file does not open\n");
-    } else {
-      printf ("File open\n");
-      for (int studentIndex = 0; studentIndex < counter; ++studentIndex) {
-	    studentsFile.getline (temp, 30, ' ');
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentSecondName);
-	    
-	    if (studentsArray[studentIndex].studentSecondName == "") {
-	      break;  
-	    }
-	    
-		studentsFile.getline (temp, 30, ' ');
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentFirstName);
-	    
-	    studentsFile.getline (temp, 3, ' ');
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentClass);
-	    
-	    studentsFile.getline (temp, 2, ' ');
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentHistoryScore);
-	    firstNumber = atoi(studentsArray[studentIndex].studentHistoryScore); // atoi = Char to integer
-	    
-	    studentsFile.getline (temp, 2, ' ');
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentMathScore);
-	    secondNumber = atoi(studentsArray[studentIndex].studentMathScore);
-	    
-	    studentsFile.getline (temp, 2);
-	    sscanf (temp, "%s", studentsArray[studentIndex].studentPhysicsScore);
-	    thirdNumber = atoi(studentsArray[studentIndex].studentPhysicsScore);
-	    
-	    studentsArray[studentIndex].studentAverageScore = (firstNumber + secondNumber + thirdNumber) / 3;
-	    
-	    printf ("%s %s %s %s %s %s %3.2lf \n",  studentsArray[studentIndex].studentSecondName,
-	    studentsArray[studentIndex].studentFirstName, studentsArray[studentIndex].studentClass, 
-	    studentsArray[studentIndex].studentHistoryScore, studentsArray[studentIndex].studentMathScore,
-	    studentsArray[studentIndex].studentPhysicsScore, studentsArray[studentIndex].studentAverageScore);
-	    
-	    if (studentsArray[studentIndex].studentAverageScore < minimum) {
-	      minimum = studentsArray[studentIndex].studentAverageScore;   
-	    }
-	    
-	  }
-	  
-	  for (int lowScoreIndex = 0; lowScoreIndex < counter; ++lowScoreIndex) {
-	    if (studentsArray[lowScoreIndex].studentAverageScore == minimum) {
-	     printf("Most failing student: %s %s \n", studentsArray[lowScoreIndex].studentSecondName,
-	     studentsArray[lowScoreIndex].studentFirstName);   
-	    }
-	    
-	  }
-	  
-      studentsFile.close();
-    }
-    
-}
-
-int main () {
   int counterMain = 0;
-  string fileName;
+  string firstFileName;
+  string secondFileName;
 
-  cout << "Name of file: ";
-  cin >> fileName;
-  
-  counterMain = lineCount(fileName);
+  cout << "Name of input file: ";
+  cin >> firstFileName;
+  cout << "Name of output file: ";
+  cin >> secondFileName;
+
+  counterMain = lineCount(firstFileName);
   if (counterMain == -1) {
-    printf ("File does not open or found");
+    printf("File does not open or found");
     return -1;
   }
-  
-  baseOfStudents(counterMain, fileName);
 
-  return 0;
+
+  ifstream studentsFile(firstFileName);
+
+  if (!studentsFile.is_open() ) {
+    printf("Error: file does not open\n");
+  } else {
+    for (int studentIndex = 0; studentIndex < counterMain; ++studentIndex) {
+      studentsFile.getline(temp, 30, ' ');
+      sscanf(temp, "%s", studentsArray[studentIndex].studentSecondName);
+
+      studentsFile.getline(temp, 30, ' ');
+      sscanf(temp, "%s", studentsArray[studentIndex].studentFirstName);
+
+      studentsFile.getline(temp, 3, ' ');
+      sscanf(temp, "%s", studentsArray[studentIndex].studentClass);
+
+      studentsFile.getline(temp, 2, ' ');
+      sscanf(temp, "%s", studentsArray[studentIndex].studentHistoryScore);
+      firstNumber = atoi(studentsArray[studentIndex].studentHistoryScore); // atoi = Char to integer
+
+      studentsFile.getline(temp, 2, ' ');
+      sscanf(temp, "%s", studentsArray[studentIndex].studentMathScore);
+      secondNumber = atoi(studentsArray[studentIndex].studentMathScore);
+
+      studentsFile.getline(temp, 2);
+      sscanf(temp, "%s", studentsArray[studentIndex].studentPhysicsScore);
+      thirdNumber = atoi(studentsArray[studentIndex].studentPhysicsScore);
+
+      studentsArray[studentIndex].studentAverageScore = (firstNumber + secondNumber + thirdNumber) / 3;
+
+      printf("%s %s %s %s %s %s %3.2lf \n", studentsArray[studentIndex].studentSecondName,
+             studentsArray[studentIndex].studentFirstName, studentsArray[studentIndex].studentClass,
+             studentsArray[studentIndex].studentHistoryScore, studentsArray[studentIndex].studentMathScore,
+             studentsArray[studentIndex].studentPhysicsScore, studentsArray[studentIndex].studentAverageScore);
+
+    if (studentsArray[studentIndex].studentAverageScore < minimum) {
+      minimum = studentsArray[studentIndex].studentAverageScore;
+      }
+
+    }
+
+    for (int lowScoreIndex = 0; lowScoreIndex < counterMain; ++lowScoreIndex) {
+      if (studentsArray[lowScoreIndex].studentAverageScore == minimum) {
+        printf("Most failing student: %s %s \n", studentsArray[lowScoreIndex].studentSecondName,
+        studentsArray[lowScoreIndex].studentFirstName);
+      }
+
+    }
+
+  studentsFile.close();
+  }
+
+  ofstream studentsFileOutput(secondFileName, ios::out);
+
+  if (!studentsFileOutput.is_open() ) {
+    printf("Error: file does not open\n");
+  } else {
+    for (int studentIndex = 0; studentIndex < counterMain; ++studentIndex) {
+      studentsFileOutput << studentsArray[studentIndex].studentSecondName;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentFirstName;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentClass;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentHistoryScore;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentMathScore;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentPhysicsScore;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << studentsArray[studentIndex].studentAverageScore;
+      studentsFileOutput << secondCrutch;
+
+      studentsFileOutput << endl;
+    }
+
+    for (int lowScoreIndex = 0; lowScoreIndex < counterMain; ++lowScoreIndex) {
+      if (studentsArray[lowScoreIndex].studentAverageScore == minimum) {
+        studentsFileOutput << firstCrutch;
+        studentsFileOutput << studentsArray[lowScoreIndex].studentSecondName;
+        studentsFileOutput << secondCrutch;
+        studentsFileOutput << studentsArray[lowScoreIndex].studentFirstName;
+      }
+
+    }
+
+  studentsFileOutput.close();
+  }
+
+    return 0;
 }
